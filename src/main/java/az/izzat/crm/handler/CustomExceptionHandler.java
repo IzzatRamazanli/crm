8cbf4d1fc8658a36785cfcdbef50d1ca790cc019
IzzatRamazanli <86556311+IzzatRamazanli@users.noreply.github.com>
2023-05-11 11:19:00 +0400

    ex logic changed

--- src/main/java/az/izzat/crm/handler/CustomExceptionHandler.java
+++ src/main/java/az/izzat/crm/handler/CustomExceptionHandler.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 import az.izzat.crm.dto.ErrorResponse;
 import az.izzat.crm.exception.BillingNegativeException;
 import az.izzat.crm.exception.RecordNotFoundException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -13,14 +14,15 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
+@RequiredArgsConstructor
 public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(RecordNotFoundException.class)
-    protected ResponseEntity<ErrorResponse> handleRecordNotFoundException(RecordNotFoundException ex, HttpStatus status,
+    protected ResponseEntity<ErrorResponse> handleRecordNotFoundException(RecordNotFoundException ex,
                                                                           HttpServletRequest request) {
         ErrorResponse errorResponse = ErrorResponse.builder()
-                .status(status.value())
-                .message(status.getReasonPhrase())
+                .status(HttpStatus.NOT_FOUND.value())
+                .message(HttpStatus.NOT_FOUND.getReasonPhrase())
                 .url(request.getContextPath())
                 .detail(ex.getMessage())
                 .date(LocalDateTime.now())
@@ -29,11 +31,11 @@ public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
     }
 
     @ExceptionHandler(BillingNegativeException.class)
-    protected ResponseEntity<ErrorResponse> handleBillingException(BillingNegativeException ex, HttpStatus status,
+    protected ResponseEntity<ErrorResponse> handleBillingException(BillingNegativeException ex,
                                                                    HttpServletRequest request) {
         ErrorResponse errorResponse = ErrorResponse.builder()
-                .status(status.value())
-                .message(status.getReasonPhrase())
+                .status(HttpStatus.BAD_REQUEST.value())
+                .message(HttpStatus.BAD_REQUEST.getReasonPhrase())
                 .url(request.getContextPath())
                 .detail(ex.getMessage())
                 .date(LocalDateTime.now())
